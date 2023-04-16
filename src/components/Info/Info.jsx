d66f0d0fd53cce3ee03cf022f3613c5d2bad0684
violeta atkinson <violeta.atkinson@gmail.com>
2023-04-16 21:16:46 +0200

    color-home

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -3,11 +3,11 @@ import fullmoon from '../../images/fullmoon.png'
 
 const Info = () => {
     return (
-        <div>
+        <div className='info'>
             <img className='fullmoon' src={fullmoon} alt='taot' height={380}/>  
             <div className='content'>
-                <h2 className='title'>Las cartas revelan aquello que quieres saber</h2>
-                <button className='tirada border-0'>ELIJE TU TIRADA</button>
+                <h2 className='title text-center'>Las cartas revelan aquello que quieres saber</h2>
+                <button className='tirada border-0'>ELIGE TU TIRADA</button>
             </div>
         </div>
     );
